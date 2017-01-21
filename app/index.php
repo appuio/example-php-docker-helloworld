c572a142a91d29e51a120c18ee8b3917c576d470
Magyarevity Miroszlav <githubcode@osadmin.com>
2017-01-21 20:38:49 +0100

    updated Hello

--- app/index.php
+++ app/index.php
@@ -53,7 +53,7 @@
     <div class="container">
 
       <div class="starter-template">
-        <h1>Hallo <?php echo 'APPUiO'?></h1>
+        <h1>Hallo <?php echo 'APPUiO code'?></h1>
         <p class="lead">APPUiO Example Dockerfile PHP</p>
       </div>
 
